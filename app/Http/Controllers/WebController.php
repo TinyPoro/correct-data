c15b1e7ccf34e9a390d19e770a05a6efb35b7c2a
tinyporo <ngophuongtuan@gmail.com>
2019-01-28 18:25:16 +0700

    fix error

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -260,46 +260,7 @@ class WebController extends Controller
             return $history;
         });
 
-        $data['histories_encode'] = $this->safe_json_encode($data['histories']);
-
         return view('welcome', ['post' => $post, 'histories' => $data['histories'], 'histories_encode' => $data['histories_encode']]);
-    }
-
-    private function safe_json_encode($value, $options = 0, $depth = 512, $utfErrorFlag = false) {
-        $value = mb_convert_encoding($value, "UTF-8", "auto");
-        $encoded = json_encode($value, $options, $depth);
-        switch (json_last_error()) {
-            case JSON_ERROR_NONE:
-                return $encoded;
-            case JSON_ERROR_DEPTH:
-                return 'Maximum stack depth exceeded'; // or trigger_error() or throw new Exception()
-            case JSON_ERROR_STATE_MISMATCH:
-                return 'Underflow or the modes mismatch'; // or trigger_error() or throw new Exception()
-            case JSON_ERROR_CTRL_CHAR:
-                return 'Unexpected control character found';
-            case JSON_ERROR_SYNTAX:
-                return 'Syntax error, malformed JSON'; // or trigger_error() or throw new Exception()
-            case JSON_ERROR_UTF8:
-                $clean = $this->utf8ize($value);
-                if ($utfErrorFlag) {
-                    return 'UTF8 encoding error'; // or trigger_error() or throw new Exception()
-                }
-                return $this->safe_json_encode($clean, $options, $depth, true);
-            default:
-                return 'Unknown error'; // or trigger_error() or throw new Exception()
-
-        }
-    }
-
-    private function utf8ize($mixed) {
-        if (is_array($mixed)) {
-            foreach ($mixed as $key => $value) {
-                $mixed[$key] = $this->utf8ize($value);
-            }
-        } else if (is_string ($mixed)) {
-            return utf8_encode($mixed);
-        }
-        return $mixed;
     }
 
     public function createPost(Request $request)
