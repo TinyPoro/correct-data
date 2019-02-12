63aef1d31339775e59c797b84b7f14a937319164
tinyporo <ngophuongtuan@gmail.com>
2019-02-13 01:22:25 +0700

    update

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -461,17 +461,17 @@
         <div class="md:flex min-h-screen">
             <div class="w-full md:w-1/2 bg-white flex items-center justify-center">
                 <div class="max-w-sm m-8">
-                    <div class="text-black text-5xl md:text-15xl font-black">
-                        404                    </div>
+                    <div class="text-black text-2xl md:text-3xl font-black">
+                        Item không tồn tại                    </div>
 
                     <div class="w-16 h-1 bg-purple-light my-3 md:my-6"></div>
 
                     <p class="text-grey-darker text-2xl md:text-3xl font-light mb-8 leading-normal">
-                        Sorry, the page you are looking for could not be found.                    </p>
+                        Chúng tôi không tìm thấy bất cứ item nào thỏa mãn id hoặc item id bạn nhập.                    </p>
 
-                    <a href="http://mywebsite.test">
+                    <a href="http://dev.data.giaingay.io/correct-data/public">
                         <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-                            Go Home
+                            Quay lại
                         </button>
                     </a>
                 </div>
