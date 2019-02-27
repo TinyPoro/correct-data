3dc8c1af7de2b8b55b22186b9ca6382c2e8b5326
tinyporo <ngophuongtuan@gmail.com>
2019-02-27 16:39:30 +0700

    authen dev server

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,26 @@
 |
  */
 
-Route::get('/', 'ToolController@index');
+Route::get('/login', '\App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', '\App\Http\Controllers\Auth\LoginController@login');
+Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout')->name('logout');
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', 'ToolController@index');
 
 // sửa
-Route::get('/post/{postId}/edit', 'ToolController@editPost')->name('post.edit');
-Route::get('/raw_history/{postId}', 'ToolController@rawHistory')->name('raw_history');;
-Route::post('/post/{postId}', 'ToolController@updatePost')->name('post.update');
+    Route::get('/post/{postId}/edit', 'ToolController@editPost')->name('post.edit');
+    Route::get('/raw_history/{postId}', 'ToolController@rawHistory')->name('raw_history');;
+    Route::post('/post/{postId}', 'ToolController@updatePost')->name('post.update');
 
 //gán nhãn
-Route::get('/post/{postId}/label', 'ToolController@editLabelPost')->name('post.edit_label');
-Route::post('/post_label/{postId}', 'ToolController@updateLabelPost')->name('post.update_label');
+    Route::get('/post/{postId}/label', 'ToolController@editLabelPost')->name('post.edit_label');
+    Route::post('/post_label/{postId}', 'ToolController@updateLabelPost')->name('post.update_label');
 
 //tạo mới
-Route::get('/post/create', 'ToolController@createPost')->name('post.create');;
-Route::post('/post', 'ToolController@storePost')->name('post.store');
+    Route::get('/post/create', 'ToolController@createPost')->name('post.create');;
+    Route::post('/post', 'ToolController@storePost')->name('post.store');
+});
